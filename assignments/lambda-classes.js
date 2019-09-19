3c48272b0c8315678fd88233f8aec9134d69a914
Kyle Corbin <kylecorbin26@gmail.com>
2019-09-19 16:22:29 -0400

    Finished assignment

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,14 +1,5 @@
 // CODE here for your Lambda Classes
-class Person{
-  constructor(name, age, location){
-    this.name = name;
-    this.age = age;
-    this.location = location;
-  }
-  speak(){
-    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
-  }
-}
+
 /////////------DOING THE SAME THING WITH OBJECT NOTATION----------------
 // class Perrson{
 //   constructor(props){
@@ -31,31 +22,119 @@ class Person{
 
 ///////////------------OBJECT TEST END------------------------------------
 
-
-//Test--------------------------------------------------------------------
-let kye = new Person('kyle', 32, 'atlanta');
-kye.speak();
-
-class Student extends Person{
-  constructor(name, age, location, special, favLang)
+class Person{
+  constructor(props){
+    this.name = props.name;
+    this.age = props.age;
+    this.location = props.location;
+  }
+  speak(){
+    console.log(`PERRSON Hello my name is ${this.name}, I am from ${this.location}`);
+  }
 }
 
+//Test--------------------------------------------------------------------
+let kye = new Person({
+  name: "karen",
+  age: 43,
+  location: "yeetland"
+})
+
+kye.speak();
+//-------------------------------------------------------------------------------
+
+class Student extends Person{
+  constructor(props){
+    super(props);
+    this.previousBackground = props.previousBackground;
+    this.className = props.className;
+    this.favSubjects = props.favSubjects;
+  }
+
+  listSubjects(){
+    this.favSubjects.forEach(function(sub){
+      console.log("one of my favorite subjects is: ",sub);
+    })
+  };
+  PRAssignment(subject){
+    console.log(`${this.name} has submitted a PR for ${subject}`);
+  };
+  sprintChallenge(subject){
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);};
+}
+
+//Test-------------------------------------
+let huck = new Student({
+  name:"Kyle",
+  age: 58,
+  location: "lagrange",
+  previousBackground: "c#",
+  className: "WEB24",
+  favSubjects: ["express", "Java", "JavaScript"]
+})
+
+huck.listSubjects();
+huck.PRAssignment("Back-end");
+huck.sprintChallenge("Front-end");
+
+//--------------------------------------------------------------------
+
 class Instructor extends Person{
-  constructor(name, age, location, special, favLang, catchPhrase){
-    super(name, age, location);
-    this.specialty = special;
-    this.FavLanguage = favLang;
-    this.catchPhrase = catchPhrase;
+  constructor(props){
+    super(props);
+    this.specialty = props.specialty;
+    this.FavLanguage = props.FavLanguage;
+    this.catchPhrase = props.catchPhrase;
   }
   demo(subject){
     console.log(`Today we are learning about ${subject}`);
   }
-  grade(Person){
-    console.log(`${Person.name} is the person I declared earlier, thier age is ${Person.age}`);
+  grade(Student,subject){
+    console.log(`${Student.name} recieves a perfect score on ${subject}`);
+  }
+  do(){
+    console.log(this.FavLanguage);
   }
 
 }
 //Test--------------------------------------------
-let yeet = new Instructor('keel', 23, 'lametown', "redux", 'C#', "YeetOrBeYate");
+let yeet = new Instructor({
+  name: "brit",
+  age: 59,
+  location: "baltimore",
+  specialty: "talking fast",
+  FavLanguage: "Python",
+  catchPhrase: "YeetOrBeYate"
+});
+
 yeet.demo("ghosts");
-yeet.grade(kye);
+yeet.grade(huck, "PHP");
+
+//-----------------------------------------------------------
+
+class ProjectManager extends Instructor{
+  constructor(props){
+    super(props);
+    this.gradClassName = props.gradClassName;
+    this.favInstructor = props.favInstructor;
+  }
+  standUp(channel){
+    console.log(`${this.name} announces to ${channel}@channel standy times!`);
+  }
+  debugsCode(Student, subject){
+    console.log(`${this.name} debugs ${Student.name}'s code on ${subject}`);
+  }
+}
+
+//Test-----------------------------
+
+let goody = new ProjectManager({
+  name: "Juan",
+  age: 99,
+  location: "Texas",
+  gradClassName: "WEBPT17",
+  favInstructor: "brit"
+});
+
+goody.standUp("JuanWEB24");
+goody.debugsCode(huck, "Python");
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
 Prototype Refactor
 
@@ -7,3 +7,271 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+// function GameObject(anything){
+//   this.createdAt = anything.createdAt;
+//   this.name = anything.name;
+//   this.dimensions = anything.dimensions;
+// }
+//
+// GameObject.prototype.destroy = function(){
+//   return `${this.name} was removed from the game`;
+// }
+
+class GameObject{
+  constructor(props){
+    this.createdAt = props.createdAt;
+    this.name = props.name;
+    this.dimensions = props.dimensions;
+  }
+  destroy(){
+    return `${this.name} was removed from the game`;
+  }
+}
+
+
+
+/*
+  === CharacterStats ===
+  * healthPoints
+  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * should inherit destroy() from GameObject's prototype
+*/
+
+  // function CharacterStats(atri){
+  //   //so basically (yeet) I'm giving GameObject and its anythings to characterstats as atri
+  //   GameObject.call(this, atri);
+  //   this.healthPoints = atri.healthPoints
+  // }
+  //
+  // //This gives CharacterStats the destroy function
+  // CharacterStats.prototype = Object.create(GameObject.prototype);
+  //
+  // CharacterStats.prototype.takeDamage = function(){
+  //   return `${this.name} took damage`;
+  // }
+
+  class CharacterStats extends GameObject{
+    constructor(props){
+      super(props);
+      this.healthPoints = props.healthPoints;
+    }
+    takeDamage(){
+      return `${this.name} took damage`;
+    }
+  }
+
+
+/*
+  === Humanoid (Having an appearance or character resembling that of a human.) ===
+  * team
+  * weapons
+  * language
+  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  * should inherit destroy() from GameObject through CharacterStats
+  * should inherit takeDamage() from CharacterStats
+*/
+
+  // function Humanoid(attri){
+  //   //giving Humanoid characterstats atributes as attri
+  //   CharacterStats.call(this, attri);
+  //   this.team = attri.team,
+  //   this.weapons = attri.weapons,
+  //   this.language = attri.language
+  //
+  // }
+  //
+  // Humanoid.prototype = Object.create(CharacterStats.prototype);
+  //
+  // Humanoid.prototype.greet = function(){
+  //   return `${this.name} offers a greeting in ${this.language}`;
+  // }
+
+  class Humanoid extends CharacterStats{
+    constructor(props){
+      super(props);
+      this.team = props.team;
+      this.weapons = props.weapons;
+      this.language = props.language;
+    }
+    greet(){
+      return `${this.name} offers a greeting in ${this.language}`;
+    }
+  }
+
+
+
+
+
+/*
+  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+  * Instances of CharacterStats should have all of the same properties as GameObject.
+*/
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
+
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
+
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  console.log("-----------STRETCH BELOW--------------");
+
+
+
+
+
+
+  // Stretch task:
+  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
+  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  // function Villain(attributes){
+  //   Humanoid.call(this, attributes);
+  //   this.battleType = attributes.battleType;
+  //   this.speed = attributes.speed;
+  // }
+  //
+  // Villain.prototype.evilAbility = function(){
+  //   this.healthPoints *= 2;
+  // }
+
+  class Villain extends Humanoid{
+    constructor(props){
+      super(props);
+      this.battleType = props.battleType;
+      this.speed = props.speed;
+    }
+    evilAbility(){
+      this.healthPoints *= 2;
+    }
+  }
+
+  // function Hero(atributes){
+  //   Humanoid.call(this, atributes);
+  //   this.battleType = atributes.battleType;
+  //   this.speed = atributes.speed;
+  // }
+  //
+  // Hero.prototype.heroAbility = function(){
+  //   this.speed *=6;
+  // }
+
+  class Hero extends Humanoid{
+    constructor(props){
+      super(props);
+      this.battleType = props.battleType;
+      this.speed = props.speed;
+    }
+    heroAbility(){
+      this.speed *=6;
+    }
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+    const henna = new Villain({
+      createdAt: new Date(),
+      dimensions: {
+        length: 6,
+        width: 6,
+        height: 6,
+      },
+      healthPoints: 20,
+      name: 'Yeet',
+      team: 'The Boys',
+      weapons: [
+        'Slatt!',
+      ],
+      language: 'Common Enlgiish',
+      battleType: "Long Distance",
+      speed: 10
+    });
+
+    const vic = new Hero({
+      createdAt: new Date(),
+      dimensions: {
+        length: 6,
+        width: 6,
+        height: 6,
+      },
+      healthPoints: 20,
+      name: 'Tracer',
+      team: 'Overwatch',
+      weapons: [
+        'pweew pweew!',
+      ],
+      language: 'british Enlgiish',
+      battleType: "Close Quarter Combat",
+      speed: 15
+    });
+
+
+    /////////////////////////////////////////////
+    console.log(`${henna.name} is a ${henna.battleType} type character`);
+    console.log(`${henna.name} had ${henna.healthPoints} healthPoints before the powerup!`);
+    henna.evilAbility();
+    console.log(`Now ${henna.name} has ${henna.healthPoints} healthPoints and is basically OP`);
+
+    console.log("But dont worry", `${vic.name} is here`);
+    console.log(`${vic.name} has a ${this.battleType} battle type.`);
+    console.log(`Her speed is ${vic.speed} before the powerup`);
+    vic.heroAbility();
+    console.log(`but now ${vic.name} has a speed of ${vic.speed} to quickly close the distance and overpower ${henna.name}`);
